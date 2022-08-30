f2ae3e568a345a2ed251625271f29076724f7225
Matthew Pfeiffer <spferical@gmail.com>
2022-08-30 01:12:33 -0400

    Support multiple pages with preact-router

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,23 @@
 import '@picocss/pico/css/pico.min.css';
-import { render, Component } from 'preact';
+// import { Component } from 'preact';
+import Router from 'preact-router';
 
-export default class App extends Component {
-    render() {
-        return (
-            <main class="container">
-                <section>
-                    <h2><a href="rps">Rock Paper Scissors</a></h2>
-                    <h2><a href="vote">Condorcet Voting</a></h2>
-                </section>
-            </main>
-        );
-    }
+function Index() {
+    return (
+        <main class="container">
+            <section>
+                <h2><a href="rps">Rock Paper Scissors</a></h2>
+                <h2><a href="vote">Condorcet Voting</a></h2>
+            </section>
+        </main>
+    )
+
+}
+
+export default function App() {
+    return (
+        <Router>
+            <Index path="/" />
+        </Router>
+    );
 }
