2c813bb52090e77502017bcac33f2992bb831ece
Matthew Pfeiffer <spferical@gmail.com>
2022-09-10 19:15:05 -0400

    Fix preact config for regular build

--- client/preact.config.js
+++ client/preact.config.js
@@ -1,12 +1,12 @@
-module.exports = function (config) {
-  if (process.env.NODE_ENV === 'development') {
-    config.devServer.proxy = [
-      {
-        context: ['/api/'],
-        target: 'ws://localhost:8000',
-        ws: true
+module.exports = function(config) {
+    if (process.env.NODE_ENV === 'development' && config.devServer) {
+        config.devServer.proxy = [
+            {
+                context: ['/api/'],
+                target: 'ws://localhost:8000',
+                ws: true
 
-      }
-    ];
-  }
+            }
+        ];
+    }
 };
