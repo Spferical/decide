5a93653889faa0008f8a241fe07e3a42385b0a9d
Matthew Pfeiffer <spferical@gmail.com>
2023-03-31 19:29:17 -0400

    Add vote loadtester

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,5 +1,7 @@
 //! Types used in decide.pfe.io public API.
-#[derive(Debug, Clone, serde::Serialize)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct CondorcetTally {
     /// totals[a][b] contains the number of votes where candidate a beat b.
     pub totals: Vec<Vec<u64>>,
@@ -8,27 +10,27 @@ pub struct CondorcetTally {
     pub ranks: Vec<Vec<usize>>,
 }
 
-#[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct VoteItem {
     pub candidate: usize,
     // Lower is better.
     pub rank: u64,
 }
 
-#[derive(Debug, serde::Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub enum ClientStatus {
     Connected,
     InvalidRoom,
 }
 
-#[derive(Debug, serde::Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct VotingResults {
     pub tally: CondorcetTally,
     pub votes: Vec<UserVote>,
 }
 
-#[derive(Debug, serde::Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct VoteView {
     pub choices: Vec<String>,
     pub your_vote: Option<UserVote>,
@@ -37,19 +39,19 @@ pub struct VoteView {
     pub results: Option<VotingResults>,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(Deserialize)]
 pub struct NewVoteForm {
     pub choices: String,
 }
 
-#[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct UserVote {
     pub name: String,
     pub selections: Vec<VoteItem>,
 }
 
 /// Data received from a client over websocket.
-#[derive(Debug, serde::Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "snake_case")]
 pub enum Command {
     Vote(UserVote),
@@ -57,7 +59,7 @@ pub enum Command {
 }
 
 /// Data serialized and sent to the client in response to a command or other change in state.
-#[derive(Debug, serde::Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct ClientNotification {
     pub status: ClientStatus,
     pub vote: Option<VoteView>,
---
+++ loadtest/src/main.rs
@@ -0,0 +1,161 @@
+use std::{
+    sync::{
+        atomic::{AtomicU64, Ordering},
+        Arc,
+    },
+    time::Instant,
+};
+
+use futures_util::{sink::SinkExt, stream::StreamExt};
+use rand::{seq::SliceRandom, Rng};
+use tokio::net::TcpStream;
+use tokio_tungstenite::{tungstenite::Message, MaybeTlsStream, WebSocketStream};
+
+use decide_api as api;
+
+type ClientStream = WebSocketStream<MaybeTlsStream<TcpStream>>;
+
+async fn make_vote(client: &mut reqwest::Client, base_url: &str) -> String {
+    let url = format!("http://{base_url}/api/start_vote");
+    let body = "choices=a%0D%0Ab%0D%0Ac";
+    let response = client.post(url).body(body).send().await.unwrap();
+    response.url().path().split('/').last().unwrap().into()
+}
+
+struct VoteClient {
+    ctrl_channel: tokio::sync::mpsc::Sender<ClientReq>,
+}
+
+#[derive(Debug)]
+enum VoteClientError {
+    Disconnected,
+}
+
+impl VoteClient {
+    #[allow(unused)]
+    async fn get_status(&self) -> Result<Arc<api::ClientNotification>, VoteClientError> {
+        let (tx, rx) = tokio::sync::oneshot::channel();
+        self.ctrl_channel
+            .send(ClientReq::GetStatus(tx))
+            .await
+            .map_err(|_| VoteClientError::Disconnected)?;
+        rx.await.map_err(|_| VoteClientError::Disconnected)
+    }
+
+    async fn vote_randomly(&self) -> Result<(), VoteClientError> {
+        let (tx, rx) = tokio::sync::oneshot::channel();
+        self.ctrl_channel
+            .send(ClientReq::VoteRandomly(tx))
+            .await
+            .map_err(|_| VoteClientError::Disconnected)?;
+        rx.await.map_err(|_| VoteClientError::Disconnected)
+    }
+}
+
+enum ClientReq {
+    GetStatus(tokio::sync::oneshot::Sender<Arc<api::ClientNotification>>),
+    VoteRandomly(tokio::sync::oneshot::Sender<()>),
+}
+
+async fn client_work(
+    init_state: api::ClientNotification,
+    mut ws: ClientStream,
+    mut chan: tokio::sync::mpsc::Receiver<ClientReq>,
+) {
+    let mut status: Arc<api::ClientNotification> = Arc::new(init_state);
+    loop {
+        tokio::select! {
+            req = chan.recv() => {
+                match req {
+                    Some(ClientReq::GetStatus(tx)) => {
+                        tx.send(status.clone()).ok();
+                    }
+                    Some(ClientReq::VoteRandomly(tx)) => {
+                        let num_candidates = status.vote.as_ref().expect("No vote active").choices.len();
+                        let command = {
+                            let mut rng = rand::thread_rng();
+                            let vote: Vec<api::VoteItem> = (0..num_candidates)
+                                .map(|candidate| api::VoteItem {
+                                    candidate,
+                                    rank: rng.gen_range(0..num_candidates as u64),
+                                })
+                                .collect();
+                            let name = String::from(*["Fred", "Joe", "???"].choose(&mut rng).unwrap());
+                            api::Command::Vote(api::UserVote{name, selections: vote})
+                        };
+                        let command = serde_json::to_string(&command).expect("Failed to serialize command");
+                        // Note: if websocket closes, it should be caught on the receive side.
+                        ws.send(Message::Text(command)).await.ok();
+                        tx.send(()).ok();
+                    }
+                    None => break
+                }
+            }
+            msg = ws.next() => {
+                match msg {
+                    Some(Ok(Message::Text(txt))) => {
+                        let notification: api::ClientNotification =serde_json::from_str(&txt).unwrap();
+                        // Errors can only happen on initial sync.
+                        assert!(matches!(notification.status, api::ClientStatus::Connected));
+                        status = Arc::new(notification);
+                    }
+                    None => break,
+                    _ => {},
+                }
+            }
+        }
+    }
+}
+
+impl VoteClient {
+    async fn connect(base_url: &str, vote_id: &str) -> Self {
+        let url = format!("ws://{base_url}/api/vote/{vote_id}");
+        let mut ws = tokio_tungstenite::connect_async(url).await.unwrap().0;
+        let (tx, rx) = tokio::sync::mpsc::channel(1);
+        // wait on initial sync
+        let init_state = loop {
+            match ws.next().await {
+                Some(Ok(Message::Text(txt))) => {
+                    break serde_json::from_str(&txt).expect("Invalid initial sync");
+                }
+                _ => {}
+            }
+        };
+
+        tokio::spawn(client_work(init_state, ws, rx));
+        VoteClient { ctrl_channel: tx }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let start = Instant::now();
+    let base_url = std::env::args().nth(1).unwrap();
+    let mut http_client = reqwest::Client::new();
+    let mut client_join_set = tokio::task::JoinSet::new();
+    let total_requests = Arc::new(AtomicU64::new(0));
+    for _ in 0..10 {
+        let vote_id = make_vote(&mut http_client, &base_url).await;
+        for _ in 0..10 {
+            let vote_client = VoteClient::connect(&base_url, &vote_id).await;
+            let total_requests_clone = Arc::clone(&total_requests);
+            client_join_set.spawn(async move {
+                loop {
+                    vote_client.vote_randomly().await.unwrap();
+                    if 1_000_000_u64 < total_requests_clone.fetch_add(1, Ordering::Relaxed) {
+                        break;
+                    }
+                }
+            });
+        }
+    }
+    while let Some(res) = client_join_set.join_next().await {
+        if let Err(err) = res {
+            eprintln!("Thread panicked: {:?}", err);
+        }
+    }
+    let end = Instant::now();
+    let duration = end - start;
+    let total_requests = total_requests.load(Ordering::Relaxed);
+    eprintln!("Performed {total_requests} requests in {duration:?}");
+}
