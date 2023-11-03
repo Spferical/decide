da87351b35d4d1219a25117a9dad48d47d99419b
Matthew Pfeiffer <spferical@gmail.com>
2023-11-02 21:05:56 -0400

    Fix npm getting bundled in docker image

--- flake.nix
+++ flake.nix
@@ -117,12 +117,15 @@
 
         packages =
           let
-            client = pkgs.buildNpmPackage {
+            clientNpmPackage = pkgs.buildNpmPackage {
               pname = "decide-client";
               version = "0.1.0";
               src = ./client;
               npmDepsHash = "sha256-HEAywFrsNZ8kBGEa58txtdujrLmdjFtzo2JISK4iCag=";
             };
+            client = pkgs.runCommand "copy" { } ''
+              cp -r ${clientNpmPackage}/lib/node_modules/decide/dist/ $out/
+            '';
           in
           {
             decide-llvm-coverage = craneLibLLvmTools.cargoLlvmCov (commonArgs // {
@@ -143,7 +146,7 @@
               config = {
                 Env = [
                   "PATH=${pkgs.dumb-init}/bin/:${decide}/bin"
-                  "DECIDE_STATIC_PATH=${client}/lib/node_modules/decide/dist"
+                  "DECIDE_STATIC_PATH=${client}"
                 ];
                 ExposedPorts = {
                   "8000/tcp" = { };
